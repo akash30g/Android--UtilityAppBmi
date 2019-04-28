6409bf548f21a174e2ba08c5856cad94c5ea2d8a
akash30g <akash.gupta@my.jcu.edu.au>
2019-04-28 22:07:23 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/bmiCal/MainActivity.java
+++ app/src/main/java/com/example/bmiCal/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.EditorInfo;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,14 +20,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-
-
 public class MainActivity extends AppCompatActivity {
 
     private final static String PREF_IS_METRIC = "system_of_unit";
-
-
-
+    private boolean flag = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,18 +52,67 @@ public class MainActivity extends AppCompatActivity {
         TextView txtResultCat = (TextView) findViewById(R.id.txt_result_cat);
         EditText txtWeight = (EditText) findViewById(R.id.editTextWeight);
         EditText txtHeight = (EditText) findViewById(R.id.editTextHeight);
+        InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(txtResult.getWindowToken(), 0);
 
             try{
 
                 double weight = Double.parseDouble(txtWeight.getText().toString());
                 double height = Double.parseDouble(txtHeight.getText().toString());
                 double result = weight / Math.pow(height, 2);
-                txtResult.setText(String.format("%.1f", result));
+                txtResult.setText(String.format("Your BMI is: "+ "%.1f", result));
                 txtResultCat.setText(getCategory(result));
+                flag = true;
 
             } catch (NumberFormatException e){
                 Log.d("Fail", "Error: " + e);
             }
 
         }
+    private String getCategory(double bmi) {
+        if (bmi < 15) {
+            return getString(R.string.bmi_cat_1);
+        }
+        if (bmi < 16) {
+            return getString(R.string.bmi_cat_2);
+        }
+        if (bmi < 18.5) {
+            return getString(R.string.bmi_cat_3);
+        }
+        if (bmi < 25) {
+            return getString(R.string.bmi_cat_4);
+        }
+        if (bmi < 30) {
+            return getString(R.string.bmi_cat_5);
+        }
+        if (bmi < 35) {
+            return getString(R.string.bmi_cat_6);
+        }
+        if (bmi < 40) {
+            return getString(R.string.bmi_cat_7);
+        }
+        if (bmi < 45) {
+            return getString(R.string.bmi_cat_8);
+        }
+        if (bmi < 50) {
+            return getString(R.string.bmi_cat_9);
+        }
+        if (bmi < 60) {
+            return getString(R.string.bmi_cat_10);
+        }
+        return getString(R.string.bmi_cat_11);
+    }
+    public void chart(View view) {
+        if (flag) {
+            TextView txtResult = (TextView) findViewById(R.id.txt_result_bmi);
+            TextView txtResultCat = (TextView) findViewById(R.id.txt_result_cat);
+            Intent intent = new Intent(this, Chart.class);
+            intent.putExtra("bmiResult", txtResult.getText());
+            intent.putExtra("bmiCat", txtResultCat.getText());
+            startActivity(intent);
+        } else {
+            Log.d("BMI: ", "BMI not calculated.");
+        }
+    }
 
+}
