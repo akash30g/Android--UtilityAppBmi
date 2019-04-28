b71c23919c477542eac78ed6c6e69949fee21a68
akash30g <akash.gupta@my.jcu.edu.au>
2019-04-28 22:07:08 +1000

    change icon, refractor code, creayed another page

--- app/src/main/java/com/example/bmiCal/Chart.java
+++ app/src/main/java/com/example/bmiCal/Chart.java
@@ -1,4 +1,24 @@
 package com.example.bmiCal;
 
-public class Chart {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Chart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chart);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            TextView txtResult = (TextView) findViewById(R.id.txt_result_bmi2);
+            txtResult.setText(extras.getString("bmiResult"));
+            TextView txtResultCat = (TextView) findViewById(R.id.txt_result_cat2);
+            txtResultCat.setText(extras.getString("bmiCat"));
+        }
+    }
+
 }
